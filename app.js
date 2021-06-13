80840adfae4d5f315181e58e9296ca7a19dfd671
carlos <carlos811009@gmail.com>
2021-06-13 18:34:55 +0800

    modify const date = ...

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const date = new Date()
+
 app.get('/', (req, res) => {
+  const date = new Date()
   res.send('列出全部 Todo')
   console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} | ${req.method} from ${req.originalUrl}`)
 })
 
 app.get('/new', (req, res) => {
+  const date = new Date()
   res.send('新增 Todo 頁面')
   console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} | ${req.method} from ${req.originalUrl}`)
 })
 
 app.get('/:id', (req, res) => {
+  const date = new Date()
   res.send('顯示一筆 Todo')
   console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} | ${req.method} from ${req.originalUrl}`)
 })
 
 app.post('/', (req, res) => {
+  const date = new Date()
   res.send('新增一筆  Todo')
   console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} | ${req.method} from ${req.originalUrl}`)
 })
