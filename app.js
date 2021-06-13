4def2ecec1e51087ceb172d7cc188e32df8cd64c
carlos <carlos811009@gmail.com>
2021-06-13 21:23:46 +0800

    add total time

--- app.js
+++ app.js
@@ -2,28 +2,39 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let start = ''
+let date = ''
+let message = ''
+app.use((req, res, next) => {
+  start = Date.now()
+  date = new Date()
+  message = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} | ${req.method} from ${req.originalUrl}`
+  next()
+})
+
 app.get('/', (req, res) => {
-  const date = new Date()
+  // console.log('end:', Date.now())
   res.send('列出全部 Todo')
-  console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} | ${req.method} from ${req.originalUrl}`)
+  const end = Date.now()
+  console.log(`${message} | total time: ${end - start} ms`)
 })
 
 app.get('/new', (req, res) => {
-  const date = new Date()
   res.send('新增 Todo 頁面')
-  console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} | ${req.method} from ${req.originalUrl}`)
+  const end = Date.now()
+  console.log(`${message} | total time: ${end - start} ms`)
 })
 
 app.get('/:id', (req, res) => {
-  const date = new Date()
   res.send('顯示一筆 Todo')
-  console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} | ${req.method} from ${req.originalUrl}`)
+  // const end = Date.now()
+  // console.log(`${message} | total time: ${end - start} ms`)
 })
 
 app.post('/', (req, res) => {
-  const date = new Date()
   res.send('新增一筆  Todo')
-  console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} | ${req.method} from ${req.originalUrl}`)
+  const end = Date.now()
+  console.log(`${message} | total time: ${end - start} ms`)
 })
 
 app.listen(port, () => {
